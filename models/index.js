773255bdf516e26b2893d4c140f7f647fb59b2b8
panicor <rachelpanico@hotmail.com>
2021-07-29 13:07:32 -0400

    added models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany( Tag, { through: "product_tag",
+foreignKey: "product_id",
+onDelete: "CASCADE"
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Products, {through: "product_tag",
+foreignKey: "tag_id",
+onDelete: "CASCADE"
+})
 
 module.exports = {
   Product,
