ad5a6663f99d3a787c12705903112f176cad2bbf
panicor <rachelpanico@hotmail.com>
2021-07-30 19:37:50 -0400

    fixed associations

--- models/index.js
+++ models/index.js
@@ -6,22 +6,36 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "category_id"
+  foreignKey: "category_id",
+  onDelete: 'CASCADE'
 })
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id"
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany( Tag, { through: "product_tag",
-foreignKey: "product_id",
-onDelete: "CASCADE"
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
 })
+
+// Product.belongsToMany(Tag, { through: {
+//   model: ProductTag,
+//   unique: false
+// },
+// as: "product_tags"
+// })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Products, {through: "product_tag",
-foreignKey: "tag_id",
-onDelete: "CASCADE"
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
 })
+// Tag.belongsToMany(Product, {through: {
+//   model: ProductTag,
+//   unique: false
+// },
+// as: "product_tags"
+// })
 
 module.exports = {
   Product,
